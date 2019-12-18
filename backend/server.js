76adac20c67156075c7e4ec681240b8a7948fae9
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-12-18 00:15:52 -0700

    fixing heroku for the 10th time 8

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,22 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  //and remove cacheing so we get the most recent comments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
 const router = express.Router();
 //Static file declaration
-app.use(express.static(path.join(__dirname, '../client/build')));
+router.get(express.static(path.join(__dirname, '../client/build')));
 //production mode
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  router.get(express.static(path.join(__dirname, '../client/build')));
   //
   // app.get('/*', (req, res) => {
   //   res.sendfile(path.join(__dirname = '../client/build/index.html'));
